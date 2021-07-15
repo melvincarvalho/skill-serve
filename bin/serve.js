97876fcea5d4262b4bc0a093cb9a13c9740cd28d
Melvin Carvalho <melvincarvalho@gmail.com>
2021-07-15 12:36:53 +0200

    add document root

--- bin/serve.js
+++ bin/serve.js
@@ -3,12 +3,18 @@
 const handler = require('serve-handler')
 const http = require('http')
 var port = 5000
+var root = '.'
+var options = {}
 try {
   const configFile = process.cwd() + '/config/skill-serve.json'
   console.log('config file', configFile)
   const config = require(configFile)
   if (config && config.port) {
     port = config.port
+  }
+  if (config && config.root) {
+    root = config.root
+    options.public = root
   }
 } catch (e) {
   console.error(e)
@@ -17,7 +23,9 @@ try {
 const server = http.createServer((request, response) => {
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/vercel/serve-handler#options
-  return handler(request, response)
+  return handler(request, response, {
+    options
+  })
 })
 
 server.listen(port, () => {
